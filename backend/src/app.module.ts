ed2ef230ffa5d18fb751638ad91dab33a7b30d46
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-12-06 19:13:55 +0100

    feat(backend): quotes service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
 import { AppController } from './app.controller';
+import { QuotesModule } from './quotes/quotes.module';
 
 @Module({
   controllers: [AppController],
-  imports: [AuthModule, DbModule],
+  imports: [AuthModule, DbModule, QuotesModule],
 })
 export class AppModule {}
