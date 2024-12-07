d468fd72621bd3b914aecd3e30a7545188d8962c
sewe2000 <serwer3103@gmail.com>
2024-12-07 01:48:55 +0100

    Fix linting in gemini service

--- backend/src/gemini/gemini.service.ts
+++ backend/src/gemini/gemini.service.ts
@@ -1,20 +1,18 @@
-import { Inject, Injectable, } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
 import { env } from 'process';
-
 
 @Injectable()
 export class GeminiService {
   private readonly genAI: GenerativeModel;
-  constructor(
-  ) {
-      const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
-      this.genAI = genAI.getGenerativeModel({
-        model: env.GEMINI_MODEL,
-      });
+  constructor() {
+    const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
+    this.genAI = genAI.getGenerativeModel({
+      model: env.GEMINI_MODEL,
+    });
   }
 
-  async generateText(prompt: string): Promise<String> {
+  async generateText(prompt: string): Promise<string> {
     const result = await this.genAI.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
