8acd9de952b7fd20dd91ea01c99db207cc5443a7
cooligus <pewienjaaa@gmail.com>
2024-12-07 07:44:26 +0100

    Fix ids in seed

--- backend/prisma/seed/article-categories.ts
+++ backend/prisma/seed/article-categories.ts
@@ -1,10 +1,10 @@
 export const articleCategories = [
     {
-        id: 'article-category-1',
+        id: 'f6a4c9a0-5a4f-4f5e-9c4a-8a8a8a8a8a8a',
         name: 'Rekreacyjne'
     },
     {
-        id: 'article-category-2',
+        id: '2b6f0a5f-3c4d-4e5f-9a6a-7a7a7a7a7a7a',
         name: 'Psychologiczne'
     }
 ]
