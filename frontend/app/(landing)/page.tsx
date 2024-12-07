7d892369a5ad755d91e4c0f02e08b490da10dad3
sewe2000 <serwer3103@gmail.com>
2024-12-07 03:34:16 +0100

    Restrict access to the dashboard

--- frontend/app/(landing)/page.tsx
+++ frontend/app/(landing)/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "@/components/landing/hero";
 import { getRandomQuote } from "@/actions/quotes";
 
-
 export default async function Home() {
   const quote = await getRandomQuote();
 
--- frontend/app/app/page.tsx
+++ frontend/app/app/page.tsx
@@ -2,8 +2,14 @@ import { getRandomQuote } from "@/actions/quotes";
 import PageContent from "./page-content";
 import { getSleepDataByDay } from "@/actions/sleep-tracker";
 import { getStreak } from "@/actions/notes";
+import { isUserLoggedIn } from "@/actions/auth";
+import {redirect} from 'next/navigation';
 
 const AppPage = async () => {
+
+    if (!await isUserLoggedIn())
+        redirect('/login');
+
     const quote = await getRandomQuote();
     const sleepData = await getSleepDataByDay({
         date: new Date(),
